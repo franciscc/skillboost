769af48ba4ce5b824d2962fc9d99b1380ce12714
franciscc <49594803+franciscc@users.noreply.github.com>
2023-04-21 20:19:19 -0300

    more styling and course service created

--- src/app/courses/models/course.class.ts
+++ src/app/courses/models/course.class.ts
@@ -2,7 +2,7 @@ export default class Course {
     constructor(
         public id : string,
         public name : string,
-        public description : string,
+        public description : string, // 285 chars max
         public durationInHours : number,
         public vacancies : number,
         public price : number
